d60a576347701d476c6e832c65b36ded6fe171c8
conan <angelomarchi05@gmail.com>
2023-03-09 18:52:38 -0300

    portifolio

---
+++ index.js
@@ -0,0 +1,6 @@
+function troggleMenu(){
+    const menuToggle = document.querySelector('.alternar');
+    const sidebar = document.querySelector('.sidebar');
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+} 
